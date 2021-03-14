a3a4f9032802b9904ffe7dbede0eb6800a5db958
zweissrock <k@dxjRTd1>
2021-03-13 23:57:58 -0500

    Fixed gameplay loop. Winner was never being set back to false.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,7 @@
 import random
 
-
 class TicTacToe:
     board = {}
-    turn = 0
-    winner = False
     game_type = ''
 
     def __init__(self):
@@ -14,6 +11,8 @@ class TicTacToe:
 
     def start_menu(self):
         in_progress = True
+        self.winner = False
+        self.turn = 0
 
         while in_progress:
             print("Shat would you like to do?")
@@ -27,15 +26,6 @@ class TicTacToe:
                     tic_tac.game_loop("human")
                 elif vs_selection.upper() == 'CPU':
                     tic_tac.game_loop("easy")
-                    # print("easy or hard?")
-                    # cpu_selection = input()
-                    # if cpu_selection.upper() == "EASY":
-                    #     tic_tac.player_input(easy_cpu)
-                    # elif cpu_selection.upper() == "HARD":
-                    #     tic_tac.player_input(hard_cpu)
-                    # else:
-                    #     print("not a valid selection")
-                    print("ok")
                     in_progress = False
                 else:
                     print("Not a valid selection.")
@@ -72,7 +62,6 @@ class TicTacToe:
             selection = input()
         elif vs_mode == "easy":
             selection = self.easy_cpu()
-        # while until we get a selection in the board
         while selection not in self.board or self.winner or self.board[selection] != ' ':
             if vs_mode == "human":
                 print("invalid character, please try again.")
@@ -80,12 +69,20 @@ class TicTacToe:
             elif vs_mode == "easy":
                 selection = self.easy_cpu()
         self.assign_space(selection)
-        print(self.board)
         if self.turn >= 5:
             self.check_for_winner()
-
     def easy_cpu(self):
         return str(random.randint(1, 9))
+
+    def hard_cpu(self):
+        """
+        rules for hard cpu
+        read the board in current state
+        if cpu can win this turn, take
+        if player is going to win, take
+        if middle space is not taken, take
+
+        """
 
     def check_for_winner(self):
         win = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3'], ['7', '4', '1'], ['8', '5', '2'], ['9', '6', '3'],
@@ -101,15 +98,18 @@ class TicTacToe:
                 if counter == 3:
                     print("X wins!")
                     self.winner = True
+                    tic_tac.clear_board()
                     tic_tac.start_menu()
-
                 elif counter == -3:
                     print("O wins!")
                     self.winner = True
+                    tic_tac.clear_board()
                     tic_tac.start_menu()
-                # debug
-                # print(counter)
-                # print("line check {}".format(x))
+
+    def clear_board(self):
+        self.board = {'7': ' ', '8': ' ', '9': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',
+                      '1': ' ', '2': ' ', '3': ' '}
 
     def assign_space(self, user_input):
         if user_input in self.board and self.board[user_input] == ' ':
@@ -126,7 +126,6 @@ class TicTacToe:
         print(self.board['4'] + " | " + self.board['5'] + " | " + self.board['6'])
         print("_________")
         print(self.board['1'] + " | " + self.board['2'] + " | " + self.board['3'])
-
 
 if __name__ == "__main__":
     tic_tac = TicTacToe()
